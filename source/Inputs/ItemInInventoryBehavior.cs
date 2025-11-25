c6784fb5bb9c0d981ec8be2968e3e8034306db36
maltiez2 <maltiez2@gmail.com>
2025-11-25 16:14:05 +0300

    wip

--- source/Inputs/ItemInInventoryBehavior.cs
+++ source/Inputs/ItemInInventoryBehavior.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.Common;
 
 namespace CombatOverhaul.Inputs;
 
@@ -11,67 +12,88 @@ public interface IOnInInventory
     void OnInInventory(EntityPlayer player, ItemSlot slot);
 }
 
-public class InInventoryPlayerBehavior : EntityBehavior
+public sealed class InInventoryPlayerBehavior : EntityBehavior
 {
     public InInventoryPlayerBehavior(Entity entity) : base(entity)
     {
         _player = entity as EntityPlayer ?? throw new Exception("This behavior should be attached only to player");
         _process = _player.Api.Side == EnumAppSide.Server || _player.PlayerUID == (_player.Api as ICoreClientAPI)?.Settings.String["playeruid"];
-        if (_process) _listenerId = entity.Api.World.RegisterGameTickListener(Update, _updateTimeMs, _updateTimeMs + entity.Api.World.Rand.Next(_updateTimeMs));
+        _timeSinceUpdate = 0 - entity.Api.World.Rand.NextSingle() * _updatePeriodSec;
+        _timeSinceReport = entity.Api.World.Rand.NextSingle() * _reportPeriodSec;
     }
 
     public override string PropertyName() => "CombatOverhaul:InInventory";
 
     private readonly EntityPlayer _player;
     internal static readonly List<long> _reportedEntities = [];
-    private const int _updateTimeMs = 1000;
-    private readonly long _listenerId = 0;
-    private bool _dispose = false;
+    private const float _updatePeriodSec = 1;
+    private const float _reportPeriodSec = 10 * 60;
     private readonly bool _process;
+    private float _timeSinceUpdate = 0;
+    private float _timeSinceReport = 0;
 
     public override void OnGameTick(float deltaTime)
     {
-        if (entity.ShouldDespawn)
+        if (!_process) return;
+
+        _timeSinceUpdate += deltaTime;
+        _timeSinceReport += deltaTime;
+
+        if (_timeSinceUpdate < _updatePeriodSec) return;
+
+        _timeSinceUpdate = 0;
+
+        Update();
+    }
+
+    private void Update()
+    {
+        if (_player?.Player?.InventoryManager?.Inventories == null) return;
+
+        foreach ((_, IInventory? inventory) in _player.Player.InventoryManager.Inventories)
         {
-            _dispose = true;
+            if (inventory == null || inventory is InventoryPlayerCreative) continue;
+
+            foreach (ItemSlot? slot in inventory)
+            {
+                try
+                {
+                    ProcessSlot(slot);
+                }
+                catch (Exception exception)
+                {
+                    if (_timeSinceReport > _reportPeriodSec)
+                    {
+                        _timeSinceReport = 0;
+                        LoggerUtil.Error(_player.Api, this, $"Error for inventory: '{inventory.ClassName}', item: '{slot?.Itemstack?.Collectible?.Code}':\n{exception}");
+                    }
+                    
+                    Debug.WriteLine(exception);
+                }
+            }
         }
     }
 
-    private void Update(float dt)
+    private void ProcessSlot(ItemSlot? slot)
     {
-        if (!_process) return;
-        
-        if (_dispose)
+        if (slot == null || slot.Empty) return;
+
+        CollectibleObject? collectible = slot.Itemstack?.Collectible;
+
+        if (collectible == null) return;
+
+        if (collectible is IOnInInventory inInventory)
         {
-            entity.Api.World.UnregisterGameTickListener(_listenerId);
+            inInventory.OnInInventory(_player, slot);
             return;
         }
 
-        
-
-        try
+        foreach (CollectibleBehavior? behavior in collectible.CollectibleBehaviors)
         {
-            if (_player?.World != null)
+            if (behavior is IOnInInventory inInventory2)
             {
-                _player.WalkInventory(ProcessSlot);
+                inInventory2.OnInInventory(_player, slot);
             }
         }
-        catch (Exception exception)
-        {
-        }
-
-        
-    }
-
-    private bool ProcessSlot(ItemSlot slot)
-    {
-        if (slot?.Empty != false) return true;
-
-        if (slot.Itemstack?.Collectible?.GetCollectibleInterface<IOnInInventory>() is IOnInInventory collectible)
-        {
-            collectible.OnInInventory(_player, slot);
-        }
-
-        return true;
     }
 }
